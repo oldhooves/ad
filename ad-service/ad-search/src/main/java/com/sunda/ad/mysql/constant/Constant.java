65ffe3ab9316de5b1755252016f485c4032018c5
laotizi <465304942@qq.com>
2019-06-11 19:35:58 +0800

    Binlog增量数据的投递

--- ad-service/ad-search/src/main/java/com/sunda/ad/mysql/constant/Constant.java
+++ ad-service/ad-search/src/main/java/com/sunda/ad/mysql/constant/Constant.java
@@ -10,12 +10,12 @@ public class Constant {
 
     private static final String DB_NAME = "ad_data";
 
-    public static class AD_PALN_TABLE_INFO {
+    public static class AD_PLAN_TABLE_INFO {
         public static final String TABLE_NAME = "ad_plan";
 
         public static final String COLUMN_ID = "id";
         public static final String COLUMN_USER_ID = "user_id";
-        public static final String COLUMN_STATUS = "plan_status";
+        public static final String COLUMN_PLAN_STATUS = "plan_status";
         public static final String COLUMN_START_DATE = "start_date";
         public static final String COLUMN_END_DATE = "end_date";
     }
@@ -25,7 +25,7 @@ public class Constant {
 
         public static final String COLUMN_ID = "id";
         public static final String COLUMN_TYPE = "type";
-        public static final String COLUMN_METERIAL_TYPE = "material_type";
+        public static final String COLUMN_MATERIAL_TYPE = "material_type";
         public static final String COLUMN_HEIGHT = "height";
         public static final String COLUMN_WIDTH = "width";
         public static final String COLUMN_AUDIT_STATUS = "audit_status";
@@ -75,7 +75,7 @@ public class Constant {
 
     static {
         table2Db = new HashMap<>();
-        table2Db.put(AD_PALN_TABLE_INFO.TABLE_NAME, DB_NAME);
+        table2Db.put(AD_PLAN_TABLE_INFO.TABLE_NAME, DB_NAME);
         table2Db.put(AD_CREATIVE_TABLE_INFO.TABLE_NAME, DB_NAME);
         table2Db.put(AD_UNIT_TABLE_INFO.TABLE_NAME, DB_NAME);
         table2Db.put(AD_CREATIVE_UNIT_TABLE_INFO.TABLE_NAME, DB_NAME);
